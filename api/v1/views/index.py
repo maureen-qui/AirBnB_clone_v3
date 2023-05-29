e402fc477b3afc12b3becf94ae99b4274feab021
maureen-qui <maureenwangui08@yahoo.com>
2023-05-29 09:22:18 -0700

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-""" Module for index.py """
+""" Initialization file for views module """
 
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
-
-classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
-           "reviews": "Review", "states": "State", "users": "User"}
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
-@app_views.route('/status', strict_slashes=False)
-def get_status_route():
-    """ Returns first json object """
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def get_obj_count_route():
-    """ retrieves the number of each objects by type """
-    count_dict = {}
-    for k, v in classes.items():
-        count_dict[k] = storage.count(v)
-    return jsonify(count_dict)
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.places import *
+from api.v1.views.users import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
